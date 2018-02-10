47a29329c5f9197b13a86a70903a6aea7aa38196
Morten Haraldsen <m.haraldsen@kezzler.com>
2018-02-10 07:15:13 +0100

    More tests for MapStore implementations

--- kfka-test/src/main/java/com/ethlo/kfka/MapStoreTest.java
+++ kfka-test/src/main/java/com/ethlo/kfka/MapStoreTest.java
@@ -22,6 +22,8 @@ package com.ethlo.kfka;
 
 import static org.fest.assertions.api.Assertions.assertThat;
 
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -40,11 +42,58 @@ public abstract class MapStoreTest
     public void testStoreAll()
     {
         final Map<Long, CustomKfkaMessage> input = new TreeMap<>();
-        input.put(123L, new CustomKfkaMessage.CustomKfkaMessageBuilder().topic("foobar").type("mytype").payload("hello").id(123L).build());
-        input.put(129L, new CustomKfkaMessage.CustomKfkaMessageBuilder().topic("foobar").type("mytype").payload("goodbye").id(129L).build());
+        final long key1 = 123;
+        final long key2 = 129;
+        input.put(key1, new CustomKfkaMessage.CustomKfkaMessageBuilder().topic("foobar").type("mytype").payload("hello").id(key1).build());
+        input.put(key2, new CustomKfkaMessage.CustomKfkaMessageBuilder().topic("foobar").type("mytype").payload("goodbye").id(key2).build());
         mapStore.storeAll(input);
         
         final Map<Long, CustomKfkaMessage> output = mapStore.loadAll(input.keySet());
         assertThat(output).isEqualTo(input);
+    }
+    
+    @Test
+    public void testStoreSingle()
+    {
+        final long id = 130L; 
+        final CustomKfkaMessage message = new CustomKfkaMessage.CustomKfkaMessageBuilder().topic("foobar").type("mytype").payload("goodbye").id(id).build();
+        mapStore.store(id, message);
+        
+        final CustomKfkaMessage output = mapStore.load(id);
+        assertThat(output).isEqualTo(message);
+    }
+    
+    @Test
+    public void loadAllKeys()
+    {
+        final long key1 = 135;
+        final long key2 = 136;
+        final Map<Long, CustomKfkaMessage> input = new TreeMap<>();
+        input.put(key1, new CustomKfkaMessage.CustomKfkaMessageBuilder().topic("foobar").type("mytype").payload("hello").id(key1).build());
+        input.put(key2, new CustomKfkaMessage.CustomKfkaMessageBuilder().topic("foobar").type("mytype").payload("goodbye").id(key2).build());
+        mapStore.storeAll(input);
+        
+        final Iterable<Long> allKeys = mapStore.loadAllKeys();
+        assertThat(allKeys).contains(135L, 136L);
+    }
+    
+    @Test
+    public void testDeleteAll()
+    {
+        final Map<Long, CustomKfkaMessage> input = new TreeMap<>();
+        final long key1 = 140;
+        final long key2 = 141;
+        final Collection<Long> keys = Arrays.asList(key1, key2);
+        
+        input.put(key1, new CustomKfkaMessage.CustomKfkaMessageBuilder().topic("foobar").type("mytype").payload("hello").id(key1).build());
+        input.put(key2, new CustomKfkaMessage.CustomKfkaMessageBuilder().topic("foobar").type("mytype").payload("goodbye").id(key2).build());
+        mapStore.storeAll(input);
+        assertThat(mapStore.load(key1)).isNotNull();
+        assertThat(mapStore.load(key2)).isNotNull();
+        
+        mapStore.deleteAll(keys);
+        
+        final Map<Long, CustomKfkaMessage> output = mapStore.loadAll(keys);
+        assertThat(output).isEmpty();
     }
 }
