e898e04d64376f2218728696be6607ccdc53dda7
Morten Haraldsen <m.haraldsen@kezzler.com>
2021-04-21 14:17:13 +0200

    Bumping version

--- kfka-core/src/main/java/com/ethlo/kfka/Assert.java
+++ kfka-core/src/main/java/com/ethlo/kfka/Assert.java
@@ -2,16 +2,16 @@ package com.ethlo.kfka;
 
 /*-
  * #%L
- * kfka-model
+ * kfka-core
  * %%
- * Copyright (C) 2017 Morten Haraldsen (ethlo)
+ * Copyright (C) 2017 - 2021 Morten Haraldsen (ethlo)
  * %%
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * 
  *      http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
